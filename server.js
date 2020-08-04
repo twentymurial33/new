60f92f68f995084f032c50b0cb10a3ffbdce7d9e
murial <mlomax@twu.edu>
2020-08-04 08:20:22 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var bodyParser= require ("body-parser");
+var app= express();
+var morgan= require("morgan");
+
+//configure app
+
+app.use(morgan('dev'));
+
+//body parser
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+//DATABASE setup 
+var mongoose= require ('mongoose');
+mongoose.connect();
+
+//Handle the connection event
+
